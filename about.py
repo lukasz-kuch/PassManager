5e15c95094d9916cb82a54c26443b39ea11e2d48
lukasz-kuch <lukas.walbrzych@gmail.com>
2022-04-14 11:25:36 +0200

    Versioning fix

--- about.py
+++ about.py
@@ -6,5 +6,6 @@ Powershell Gallery: example@com"""
 versions_log = """1.0.0 - initial version:
 add/delete/update groups, add/delete/update entries
 1.0.1 - adding Azure theme/style
+1.1.1 - adding 'drag and move' action for folders/groups
 """
-version = '1.0.1'
+version = '1.1.1'
