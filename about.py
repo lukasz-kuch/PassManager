415bc9910acbc1d8e0d63b948b31cb22df5bf77a
Lukasz <lukas.walbrzych@gmail.com>
2022-04-14 11:27:12 +0200

    Add 'move group' action (#1)
    * Adding 'drag and move' action for folders/goups
    
    * Versioning fix

--- about.py
+++ about.py
@@ -3,7 +3,9 @@ Github: example@com
 Docker: example@com
 Powershell Gallery: example@com"""
 
-version = """1.0.0 - initial version:
+versions_log = """1.0.0 - initial version:
 add/delete/update groups, add/delete/update entries
 1.0.1 - adding Azure theme/style
+1.1.1 - adding 'drag and move' action for folders/groups
 """
+version = '1.1.1'
