03ebe6a173691c9ab4c1cf0d756a474c424b7b27
Lukasz <lukas.walbrzych@gmail.com>
2022-04-15 10:36:24 +0200

    Change versionig (#3)

--- about.py
+++ about.py
@@ -3,9 +3,8 @@ Github: https://github.com/lukasz-kuch
 Docker: example@com
 Powershell Gallery: example@com"""
 
-versions_log = """1.0.0 - initial version:
-add/delete/update groups, add/delete/update entries
-1.0.1 - adding Azure theme/style
-1.1.1 - adding 'drag and move' action for folders/groups
+versions_log = """0.1.0 - add/delete/update groups, add/delete/update entries
+0.1.1 - adding Azure theme/style
+0.1.2- adding 'drag and move' action for folders/groups
 """
-version = '1.1.1'
+version = '0.1.2'
