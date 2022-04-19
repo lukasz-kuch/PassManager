0c1e3fb42876f6902fe25f447a7d86e8dfe1f7ea
Lukasz <lukas.walbrzych@gmail.com>
2022-04-19 09:10:39 +0200

    Update version (#6)

--- about.py
+++ about.py
@@ -4,7 +4,8 @@ Docker: example@com
 Powershell Gallery: example@com"""
 
 versions_log = """0.1.0 - add/delete/update groups, add/delete/update entries
-0.1.1 - adding Azure theme/style
-0.1.2- adding 'drag and move' action for folders/groups
+0.1.1 - add Azure theme/style
+0.1.2- add 'drag and move' action for folders/groups
+0.2.0 - add registry option when start the app at the first time
 """
-version = '0.1.2'
+version = '0.2.0'
